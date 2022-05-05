5f78c03211c1c2caf6ac1b27bf581df7ed37248c
nicola <sieben.nicola@gmail.com>
2022-05-05 20:30:00 +0200

    plop

--- f2f_frontend/src/components/Partials/Header/Header.js
+++ f2f_frontend/src/components/Partials/Header/Header.js
@@ -5,6 +5,8 @@ function Header(){
     // --------------------------- VARIABLES ------------------------
 
 
+
+
     // --------------------------- FUNCTIONS ------------------------
 
 
@@ -13,16 +15,16 @@ function Header(){
       <div class="row heading">
         <div class="col-sm-2 col-md-1 col-xs-3 heading-avatar">
           <div class="heading-avatar-icon">
-            <img src="./img/BOT.png"/>
+            <img src="./img/BOT.png" class='img-fluid'/>
           </div>
         </div>
         <div class="col-sm-8 col-xs-7 heading-name">
-          <a class="heading-name-meta fw-bold">Helpus
+          <a class="heading-name-meta fw-bold fs-1">Helpus
           </a>
           <span class="heading-online">Online</span>
         </div>
         <div class="col-sm-1 col-xs-1  heading-dot pull-right">
-          <i class="fa-solid fa-xmark pull-right" aria-hidden="true"></i>
+        <a href="https://www.proximus.be/fr/id_personal/particuliers.html?v1=paidsearch&v3=google&v4=brand&v5=&v6=proximus&v7=res-brand-proximus&ds_rl=1286644&gclid=CjwKCAjw682TBhATEiwA9crl36BJJqozYS17orgcCiD8rXFxox2UFp65woBex5BivBvgQ7RJuXWehBoCbVoQAvD_BwE&gclsrc=aw.ds"><i class="fa-solid fa-xmark fa-2x pull-right" aria-hidden="true"></i></a>
         </div>
       </div>
     );
--- f2f_frontend/src/components/Partials/TextBox/Textbox.js
+++ f2f_frontend/src/components/Partials/TextBox/Textbox.js
@@ -15,13 +15,14 @@ function TextBox(){
             <i class="fa fa-smile-o fa-2x"></i>
           </div>
           <div class="col-sm-9 col-xs-9 reply-main">
-            <textarea class="form-control" rows="1" id="comment"></textarea>
+            <textarea class="form-control" placeholder="Type a message" rows="1" id="comment"></textarea>
           </div>
           <div class="col-sm-1 col-xs-1 reply-recording">
             <i class="fa fa-microphone fa-2x" aria-hidden="true"></i>
+            
           </div>
           <div class="col-sm-1 col-xs-1 reply-send">
-          <i class="fa-solid fa-share fa-2x"></i>
+          <i class="fa-solid fa-angle-right fa-2x"></i>
           </div>
         </div>
     );
